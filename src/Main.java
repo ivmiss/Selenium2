09ce4c36781c979479532af3e3dfa6a517cfe292
Ivana <ivana.vujic.milic@gmail.com>
2018-06-18 22:19:18 +0200

    Created Select method for combobox type of input elements

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+package main;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
@@ -18,16 +20,16 @@ public class Main {
         WebElement locationField = wait.until(ExpectedConditions.elementToBeClickable(By.id("ss")));
         locationField.sendKeys("Belgrade");
 
-        WebElement locationBar = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]")));
+        WebElement locationBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"frm\"]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]")));
         locationBar.click();
 
-        WebElement checkInPicker = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[3]/div/div/div[1]/table/tbody/tr[3]/td[6]/span")));
+        WebElement checkInPicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"frm\"]/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[3]/div/div/div[1]/table/tbody/tr[4]/td[1]/span")));
         checkInPicker.click();
 
         WebElement datePicker = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-calendar2-type=\"checkout\"")));
         datePicker.click();
 
-        WebElement checkOutPicker = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[2]/div/div[3]/div/div/div/div[2]/div[2]/div[3]/div/div/div[1]/table/tbody/tr[5]/td[6]/span")));
+        WebElement checkOutPicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"frm\"]/div[1]/div[2]/div/div[3]/div/div/div/div[2]/div[2]/div[3]/div/div/div[1]/table/tbody/tr[5]/td[6]/span")));
         checkOutPicker.click();
         
         WebElement comboField = wait.until(ExpectedConditions.elementToBeClickable(By.id("xp__guests__toggle")));
@@ -36,31 +38,43 @@ public class Main {
         WebElement noRooms = wait.until(ExpectedConditions.elementToBeClickable(By.id("no_rooms")));
         noRooms.click();
         
-        WebElement noRoomsPicker = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#no_rooms > option:nth-child(4)")));
-        noRoomsPicker.click();
+//        WebElement noRoomsPicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#no_rooms > option:nth-child(4)")));
+//        noRoomsPicker.click();
+        
+        Select noRoomsPicker = new Select(wait.until(ExpectedConditions.elementToBeClickable(By.id("no_rooms"))));
+        noRoomsPicker.selectByValue("2");
         
         WebElement noAdults = wait.until(ExpectedConditions.elementToBeClickable(By.id("group_adults")));
         noAdults.click();
         
-        WebElement noAdultsPicker = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#group_adults > option:nth-child(8)")));
-        noAdultsPicker.click();
-        
+//        WebElement noAdultsPicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#group_adults > option:nth-child(8)")));
+//        noAdultsPicker.click();
+
+        Select noAdultsPicker = new Select(wait.until(ExpectedConditions.elementToBeClickable(By.id("group_adults"))));
+        noAdultsPicker.selectByValue("8");
+
         WebElement noChildren = wait.until(ExpectedConditions.elementToBeClickable(By.id("group_children")));
         noChildren.click();
         
-        WebElement noChildrenPicker = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#group_children > option:nth-child(3)")));
-        noChildrenPicker.click();
+//        WebElement noChildrenPicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#group_children > option:nth-child(3)")));
+//        noChildrenPicker.click();
+
+        Select noChildrenPicker = new Select(wait.until(ExpectedConditions.elementToBeClickable(By.id("group_children"))));
+        noChildrenPicker.selectByValue("2");
         
         WebElement childrenAgeBox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[3]/div[2]/div/div/div[4]/select[1]")));
         childrenAgeBox.click();
            
-        WebElement noChildrenAgePicker = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[3]/div[2]/div/div/div[4]/select[1]/option[14]")));
-        noChildrenAgePicker.click();
+//        WebElement noChildrenAgePicker = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"frm\"]/div[1]/div[3]/div[2]/div/div/div[4]/select[1]/option[14]")));
+//        noChildrenAgePicker.click();
+        
+        Select noChildrenAgePicker = new Select(wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"frm\"]/div[1]/div[3]/div[2]/div/div/div[4]/select[1]"))));
+        noChildrenAgePicker.selectByValue("12");
         
         WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".sb-searchbox__button")));
         searchButton.click();
                 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         driver.quit();
 
     }
